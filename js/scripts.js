b127dc3d301897a233411e12a74fcc4ec06a5fa7
Adebayo Adeyeye Godwin <adebayoadeyeye889@gmail.com>
2024-05-09 12:01:15 +0100

    doing and initializng my todo list

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+function ToDoList(){
+    this.tasks = [];
+    this.completedTasks = [];
+}
+
+// method to add a task as completed
+ToDoList.prototype.addTask= function(task) {
+    this.tasks.push(tasks);
+}
+
+// method to mark a task as completed
+ToDoList.prototype.completeTask = function(taskIndex) {
+    if (taskIndex >= 0 && taskIndex < this.tasks.length) {
+        const completedTask = this.tasks.splice(taskIndex, 1)[0];
+        this.completedTasks.push(completedTask);
+    }
+    else {
+        console.log("Invalid task index");
+    }
+}
+
+
+// method to display all tasks in the todo list
+ToDoList.prototype.displayTasks = function () {
+    console.log("Tasks:");
+    this.tasks.forEach((task, index) => {
+        console.log(`${index + 1}. ${task}`);
+    });
+    console.log("Completed Tasks:");
+    this.completedTasks.forEach((task, index) => {
+        console.log(`${index + 1}. ${task}`);
+    });
+}
+
+// examples
+let myTodoList = new TodoList();
+myTodoList.addTask("Finish homework");
+myTodoList.addTask("Buy Food and Clothes");
+myTodoList.addTask("Finsih Reading");
+myTodoList.completeTask(1);
+myTodoList.displayTasks();
